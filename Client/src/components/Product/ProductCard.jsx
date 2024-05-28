cdb1b6922cb45887b9e5e93673359d024a287d7f
MatiJVillagran <matiassjv@gmail.com>
2024-05-28 19:43:19 -0300

    Componente ProductCard y ProductList funcionando

--- Client/src/components/Product/ProductCard.jsx
+++ Client/src/components/Product/ProductCard.jsx
@@ -1,7 +1,20 @@
-export default function ProductCard({allItems}) {
+import { Link } from "react-router-dom";
+import style from './ProductCard.module.css'; // Asegúrate de que este archivo CSS exista y esté correcto
+
+export default function ProductCard({ id, name, description, date, price, quantity, available, average_mark, status, id_review, id_discounts, id_store }) {
   return (
-    <>
-      ProductCard
-    </>
-  )
-};
+    <div className={style.tarjeta}>
+      <Link to={`/product/${id}`}><h2 className={style.nameSobreImagen}>{name}</h2></Link>
+      <h2>{description}</h2>
+      <h2>{date}</h2>
+      <h2>{price}</h2>
+      <h2>{quantity}</h2>
+      <h2>{available}</h2>
+      <h2>{average_mark}</h2>
+      <h2>{status}</h2>
+      <h2>{id_review}</h2>
+      <h2>{id_discounts}</h2>
+      <h2>{id_store}</h2>
+    </div>
+  );
+}
--- Client/src/components/ProductList/ProductList.jsx
+++ Client/src/components/ProductList/ProductList.jsx
@@ -1,7 +1,17 @@
-export default function ProductList() {
-    return (
-      <>
-        ProductList
-      </>
-    )
-};
+import ProductCard from "../Product/ProductCard";
+import style from './ProductList.module.css'; // Asegúrate de que este archivo CSS exista y esté correcto
+
+export default function ProductList({ allProducts }) {
+  return (
+    <div className={style.contenedor}>
+      {allProducts.map(product => (
+        <ProductCard 
+        key={product.id} 
+        id={product.id}
+        name={product.name}
+        description={product.description}
+        price={product.price} />
+      ))}
+    </div>
+  );
+}
