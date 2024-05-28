40ad0786d22f7ac1739ef413ef53e890f93bd892
niveyrojulian <niveyrojulian5@gmail.com>
2024-05-28 20:53:14 -0300

    Agrego el manejo de validacion en tiempo real de login

--- Client/src/components/UserForm/UserFormLogin.jsx
+++ Client/src/components/UserForm/UserFormLogin.jsx
@@ -1,23 +1,63 @@
+import { useState } from "react";
+import validationLogin from "./validationLogin"; // Importa tu función de validación
+
 export default function UserFormLogin({ title }) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    validationLogin({ ...formData, [name]: value }, errors, setErrors);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validationLogin(formData, errors, setErrors);
+    const noErrors = Object.keys(errors).every((key) => errors[key] === "");
+
+    if (noErrors) {
+      console.log("Form data submitted:", formData);
+    }
+  };
+
   return (
     <div className="w-full max-w-xs">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
         <h1 className="text-center mb-4 text-3xl text-primary border-b-2 p-2">
           <strong>{title}</strong>
         </h1>
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="username"
+            htmlFor="email"
           >
-            Username
+            Email
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username"
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+              errors.email ? "border-red-500" : ""
+            }`}
+            id="email"
             type="text"
-            placeholder="Username"
+            placeholder="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
           />
+          {errors.email && (
+            <p className="text-red-500 text-xs italic">{errors.email}</p>
+          )}
         </div>
         <div className="mb-6">
           <label
@@ -27,16 +67,24 @@ export default function UserFormLogin({ title }) {
             Password
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ${
+              errors.password ? "border-red-500" : ""
+            }`}
             id="password"
             type="password"
             placeholder="******************"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
           />
+          {errors.password && (
+            <p className="text-red-500 text-xs italic">{errors.password}</p>
+          )}
         </div>
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            type="submit"
           >
             Sign In
           </button>
