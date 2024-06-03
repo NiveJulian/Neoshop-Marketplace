cb6246102fe996e1adbe7ff9dedbe8fd0f655404
MatiJVillagran <matiassjv@gmail.com>
2024-06-03 01:04:03 -0300

    paginacion funcionando tailwind

--- Client/src/components/Filter/FilterStore.jsx
+++ Client/src/components/Filter/FilterStore.jsx
@@ -1,9 +1,10 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { storeFilter, clearFilteredProducts, renderCondition } from "../../Redux/Actions/Actions";
 import style from "./FilterCat.module.css"
 
 export default function FilterStore() {
   const dispatch = useDispatch();
+  const stores = useSelector((state) => state.store)
 
   const handleShopChange = (event) => {
     const store = event.target.value; 
@@ -21,12 +22,9 @@ export default function FilterStore() {
     <div className={style.font}>
       <select onChange={handleShopChange}>
         <option value="All">All</option>
-        <option value="Ferreteria Lopez">Ferr Lopez</option>
-        <option value="Ferreteria Martinez">Ferr Martinez</option>
-        <option value="Falavella">Falavella</option>
-        <option value="Musimundo">Musimundo</option> 
-        <option value="Fravega">Fravega</option> 
-        <option value="CompraGamer">CompraGamer</option>
+        {stores.map((store) => (
+          <option key={store.name} value={store.name}>{store.name}</option>
+        ))}
       </select>
     </div>
   );
