2f293b38058b78417b053e959b3709908899f9e6
niveyrojulian <niveyrojulian5@gmail.com>
2024-06-13 20:31:51 -0300

    Cambios para funcionamiento del dashboard en deploy

--- dashboard/pages/products/edit/[...id].js
+++ dashboard/pages/products/edit/[...id].js
@@ -1,5 +1,5 @@
-import Layout from "@/components/Layout";
-import ProductForm from "@/components/ProductForm";
+import Layout from "../../../components/Layout";
+import ProductForm from "../../../components/ProductForm";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
