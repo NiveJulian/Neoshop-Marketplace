655e34456ec517225cf7f729e9d606f4633584d6
niveyrojulian <niveyrojulian5@gmail.com>
2024-06-28 20:46:26 -0300

    rutaback

--- Client/src/Redux/Actions/rutaBack.js
+++ Client/src/Redux/Actions/rutaBack.js
@@ -1,4 +1,4 @@
 // rutaBack.js
-// const rutaBack = "https://neoshop-back.onrender.com";
-const rutaBack = "http://localhost:3001";
+const rutaBack = "https://neoshop-back.onrender.com";
+// const rutaBack = "http://localhost:3001";
 export default rutaBack;
