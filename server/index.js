6b873ad931860c85c666d15c4cd7c20285ca047b
niveyrojulian <niveyrojulian5@gmail.com>
2024-08-23 13:39:52 -0300

    Copiamos backend de organization proyecto-final

--- server/index.js
+++ server/index.js
@@ -2,15 +2,19 @@ require("dotenv").config();
 const port = process.env.PORT || 3001;
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const montarUsers = require("./src/utils/montarUsers.js");
+// const montarUsers = require("./src/utils/montarUsers.js");
+
+
 
 conn
-  .sync({ force: true }) //cambiar a force para trabajar localmente, alter el otro
-  .then(async () => {
+  .sync({ alter: true })//cambiar a force para trabajar localmente, alter el otro
+  .then(() => {
     server.listen(port, () => {
       console.log(`Server listening on port ${port}`);
     });
-    await montarUsers();
-    // setInterval(sendReminderCart, 2 * 60 * 1000);
+    // await montarUsers();
+    //setInterval(sendReminderCart, 2 * 60 * 1000);
   })
-  .catch((error) => console.error("Database connection error:", error));
+  .catch((error) =>
+    console.error("Database connection error:", error)
+  );
